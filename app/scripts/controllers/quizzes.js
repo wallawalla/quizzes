b57e35c1352a17b9990e9da6e9e9fb80fea79108
wallawalla <wallawalla7@gmail.com>
2014-04-23 18:30:43 +0200

    add swiper + isotope + resources

--- app/scripts/controllers/quizzes.js
+++ app/scripts/controllers/quizzes.js
@@ -5,6 +5,17 @@ angular
     .controller('QuizzesCtrl', function ($scope, $http, $routeParams) {
         $http.get('data/quizzes.json').success(function (data) {
             $scope.quizzes = data;
+            setTimeout(function () {
+                $scope.initIsotope();
+            });
         });
+
         $scope.theme = $routeParams.id;
+
+        $scope.initIsotope = function () {
+            $('.quizzes').isotope({
+                itemSelector: '.item-isotope',
+                layoutMode: 'fitRows'
+            });
+        }
     });
--- app/scripts/controllers/themes.js
+++ app/scripts/controllers/themes.js
@@ -3,14 +3,27 @@
 angular
     .module('quizzesApp')
     .controller('ThemesCtrl', function ($scope, $http) {
-        $scope.swiper = new Swiper('#themes-swiper', {
-            mode:'horizontal',
-            loop:true,
-            slidesPerView:'auto'
-        });
-
         $http.get('data/themes.json').success(function (data) {
             $scope.themes = data;
-            $scope.swiper.reInit();
+            setTimeout(function () {
+                $scope.initSwiper();
+            }, 200);
         });
+
+        $scope.initSwiper = function () {
+            if(!$scope.swiper) {
+                $scope.swiper = new Swiper('#themes-swiper', {
+                    mode:'horizontal',
+                    loop:true,
+                    slidesPerView:'auto',
+                    tdFlow: {
+                        rotate:0,
+                        stretch:0,
+                        depth:100,
+                        modifier:1,
+                        shadows:true
+                    }
+                });
+            }
+        }
     });
