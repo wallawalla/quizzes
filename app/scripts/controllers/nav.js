e1bf06fe4f01ef2681464b1a4b80c3dbcff434ed
wallawalla <wallawalla7@gmail.com>
2014-04-24 16:31:32 +0200

    update design

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -6,4 +6,13 @@ angular
         $http.get('data/menu.json').success(function (data) {
             $scope.sections = data.en;
         });
+
+        $('#nav').on('click', 'a', function (evt) {
+            evt.preventDefault();
+
+            $('body').fadeOut(function () {
+                window.location.href = $(evt.target).parent().attr('href');
+                $(this).fadeIn();
+            });
+        });
     });
